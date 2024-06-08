f7ef3246f46509a7696730182557964889be1b03
Muhammed Cindioglu <mhdcindioglu@gmail.com>
2024-06-08 20:00:52 +0300

    Add Blazored (LocalStorge, Modal, Toast)

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,6 @@
+using Blazored.LocalStorage;
+using Blazored.Modal;
+using Blazored.Toast;
 using bShop.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,7 +11,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient("bShop.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddBlazoredModal();
+builder.Services.AddBlazoredToast();
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("bShop.ServerAPI"));
 
 await builder.Build().RunAsync();
