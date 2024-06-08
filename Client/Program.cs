b2b1216ed2a7764b5b9ef31f0ec31e39f8311791
Muhammed Cindioglu <mhdcindioglu@gmail.com>
2024-06-08 12:08:03 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+using bShop.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("bShop.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("bShop.ServerAPI"));
+
+await builder.Build().RunAsync();
